b2a0b77895880d01c2a33cf176cbaf46dc375bec
Magd111 <magdkoraby@gmail.com>
2025-01-24 21:12:41 +0200

    thr commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React , {useState} from 'react';
 import logo from '../../assets/avataaars.svg'
+import '.././Line/Line'
 
 export default function Home() {
     return (
@@ -9,6 +10,7 @@ export default function Home() {
             <img className='w-100 d-block' src={logo} alt="logo" />
             </div>
             <h2 className='text-center fs-1 fw-bold text-white text-uppercase'>start Framework</h2>
+            <Line/>
             <p className='text-white '>Graphic Artist - Web Designer - Illustrator</p>
         </div>
         </>
